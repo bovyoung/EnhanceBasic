6ac0c51e6bd253b05aaed717d026a274a379fcc5
bovyoung <bovyoung@gmail.com>
2016-11-16 15:35:07 +0800

    添加 SDCard 相关方法

--- EnhanceLibrary/src/main/java/com/bowyoung/enhancelibrary/utils/SdCardUtils.java
+++ EnhanceLibrary/src/main/java/com/bowyoung/enhancelibrary/utils/SdCardUtils.java
@@ -1,10 +1,56 @@
 package com.bowyoung.enhancelibrary.utils;
 
+import android.os.Environment;
+import android.os.StatFs;
+
 /**
  * Created by zhangboo on 2016/11/16.
  * <p>
- * Desc:
+ * Desc: SdCard 工具类
  */
 
-public class SdCardUtils {
+public class SDCardUtils {
+
+    /**
+     * 是否存在 SDCard
+     *
+     * @return
+     */
+    public static boolean hasSDCard() {
+        boolean hasSDCard = false;
+        if (Environment.MEDIA_MOUNTED.endsWith(Environment.getExternalStorageState())) {
+            hasSDCard = true;
+        } else {
+            hasSDCard = false;
+        }
+        return hasSDCard;
+    }
+
+    public static String getSDCardPath() {
+        if (hasSDCard()) {
+            return Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
+        }
+        return "/sdcard/";
+    }
+
+    public static boolean isSDCardWrite() {
+        return Environment.getExternalStorageDirectory().canWrite();
+    }
+
+    public static boolean hasSDCardAndCanWrite() {
+        return hasSDCard() && isSDCardWrite();
+    }
+
+    public static long getSDCardAvailableBlocks() {
+        if (hasSDCardAndCanWrite()) {
+            String path = getSDCardPath();
+            if (path != null) {
+                StatFs statFs = new StatFs(path);
+                long blockSize = statFs.getBlockSizeLong();
+                long availaBlock = statFs.getAvailableBlocksLong();
+                return blockSize * availaBlock;
+            }
+        }
+        return 0;
+    }
 }
