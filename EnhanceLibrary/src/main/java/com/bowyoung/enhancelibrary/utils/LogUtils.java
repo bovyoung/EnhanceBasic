e9ef178045b2fa79609a6a836bb06cb9427f546c
bovyoung <bovyoung@gmail.com>
2016-11-23 10:37:23 +0800

    edit LogUtils

--- EnhanceLibrary/src/main/java/com/bowyoung/enhancelibrary/utils/LogUtils.java
+++ EnhanceLibrary/src/main/java/com/bowyoung/enhancelibrary/utils/LogUtils.java
@@ -9,41 +9,52 @@ import com.bowyoung.enhancelibrary.config.AppConfig;
  */
 public class LogUtils {
 
-    private static final String TAG = "LogUtils";
+    private static final String TAG = LogUtils.class.getName();
     public static boolean mIsDebug = AppConfig.IS_DEBUG;
 
     public static void setDebug(boolean isDebug) {
         if (isDebug) {
-            i("LogUtils", "log is open");
+            i(TAG, "log is open");
         } else {
-            i("LogUtils", "log is close");
+            i(TAG, "log is close");
         }
         mIsDebug = isDebug;
     }
 
     public static void d(String tag, Object object) {
-        if (mIsDebug)
+        if (mIsDebug) {
             Log.d(tag, object.toString());
+        }
     }
 
     public static void d(String tag, Object object, Throwable tr) {
-        if (mIsDebug)
+        if (mIsDebug) {
             Log.d(tag, object.toString(), tr);
+        }
     }
 
     public static void e(String tag, Object object) {
-        if (mIsDebug)
+        if (mIsDebug) {
             Log.e(tag, object.toString());
+        }
     }
 
     public static void e(String tag, Object object, Throwable tr) {
-        if (mIsDebug)
+        if (mIsDebug) {
             Log.e(tag, object.toString(), tr);
+        }
     }
 
     public static void i(String tag, Object object) {
-        if (mIsDebug)
+        if (mIsDebug) {
             Log.i(tag, object.toString());
+        }
+    }
+
+    public static void w(String tag, Object object) {
+        if (mIsDebug) {
+            Log.w(tag, object.toString());
+        }
     }
 
 }
